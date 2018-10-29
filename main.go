2cad8ace0c79623802f0f69d31c2f93718ac4986
Dan Ioan <darkcomputer@gmail.com>
2018-10-29 14:40:38 +0200

    get cities list

--- main.go
+++ main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	//"fmt"
-	"net/http"
-
+	"fmt"
+	"github.com/dan-ancora/apiteamamerica"
 	"google.golang.org/appengine" // Required external App Engine library
+	"net/http"
 )
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
@@ -16,10 +16,39 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	//fmt.Fprintln(w, "Hello, Google Api Engine!")
+	fmt.Fprintln(w, "<h1>Hello, Google Api Engine!</h1>")
+}
+
+func getCitiesList(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Start testing Team America Package")
+
+	taClient := &apiteamamerica.Taclient{
+		Username: "darkcomputer",
+		Password: "12345",
+		URL:      "https://javatest.teamamericany.com:8443/TADoclit/services/TADoclit?wsdl"}
+	//URL: "http://82.77.142.41:9999/ancoraerp/index.jsp"}
+
+	/*
+		ok, err := taClient.Connect()
+		if err != nil {
+			fmt.Println("Error connecting to url: ", taClient.URL)
+		} else {
+			fmt.Println("Response: ", ok)
+		}
+	*/
+
+	response, err := taClient.ListCities(r)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		w.Write(response)
+	}
+
 }
 
 func main() {
+	http.HandleFunc("/city_list", getCitiesList)
+
 	http.HandleFunc("/", indexHandler)
 	appengine.Main() // Starts the server to receive requests
 }
